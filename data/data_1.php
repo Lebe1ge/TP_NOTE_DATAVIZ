b4ba46d50fbc32e49040bf5a31c8fe36667e1087
Ant1-D <a.delelis@hotmail.fr>
2016-06-08 18:26:13 +0200

    Question 1

--- data/data_1.php
+++ data/data_1.php
@@ -1,6 +1,15 @@
 <div class="inner cover">
-    <h2>Test</h2>
-    <div id="d3test">
-        <p></p>
-    </div>
+    <h2>Question n°1</h2>
+    <!-- PieChart 1 -->
+	<div id="date_axis"></div>
+	<div class="explication">
+		<h3>Evolution du nombre d’amis au fil du mois.</h3>
+		<p>
+			(2 points, JQplot Date Axis).
+			(si vous faites cette question, choisissez un utilisateur 
+			intéressant avec beaucoup d’amis pour faire vos tests.
+			Pour se faire n’hésitez pas à requêter sur la base pour 
+			trouver ce que vous cherchez)
+		</p>
+	</div>
 </div>
