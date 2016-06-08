5e75b35f43ae154c20a31b9433220368ab3907a9
bobby <jeremy.felicite@gmail.com>
2016-06-08 20:09:46 +0200

    add popularite

---
+++ webservices/popularite.php
@@ -0,0 +1,36 @@
+<?php
+	// Le tableau de résultat
+	$result_request = array();
+
+	/*
+		On teste si le paramètre GET existe
+		0 -> tous les utilisateurs
+		id_unique -> un seul utilisateur
+		plusieurs id séparés par des virgules -> plusieurs utilisateurs
+	*/
+	if(isset($_GET['user'])) {
+
+		// Connexion à la BDD
+		include("../bdd/connexion_bdd.php");
+
+		$user = $_GET['user'];
+    $note = 0;
+    $month = date('Y-m');
+    $end_date = date("t", strtotime($a_date));
+
+    for( $i = 1; $i <= $end_date; $i++  ){
+      $query = "SELECT ROUND(AVG(note.note)) as note, note.date as date_popularite FROM notations note
+  					            LEFT JOIN photos photo ON note.photo = photo.id
+                        LEFT JOIN utilisateurs us ON us.photo = photo.id
+  					            WHERE us.id = " . $user . " AND note.date = '" . $month . "-" . $i . "'" ;
+
+      $result = mysqli_query($conn, $query);
+      $res = mysqli_fetch_assoc($result);
+      $result_request[] = [$res['date_popularite'], (int)$res['note']];
+
+    }
+		// Déconnexion de la BDD
+		include("../bdd/deconnexion_bdd.php");
+	}
+  echo json_encode($result_request);
+?>
